bbd8032f8de8051acbdafea82c13a9169c5c3960
lazyCodes7 <rishabmudliar@gmail.com>
2020-11-13 19:09:43 +0530

    Added Week1 assignment-1

---
+++ Week1/Assignments/AdapterPattern.java
@@ -0,0 +1,37 @@
+/* CoffeeMachineInterface
+*/
+interface CoffeeMachineInterface {
+
+    void chooseFirstSelection();
+    
+    void chooseSecondSelection();
+    
+}
+/* OldCoffeeMachine */
+class OldCoffeeMachine {
+
+    void selectA(){
+    System.out.println("YOu have selected OptionA");
+    }
+    void selectB(){
+    System.out.println("YOu have selected OptionB");
+    }
+    
+    
+}
+/* CoffeeTouchScreenAdapter */
+class CoffeeTouchscreenAdapter implements CoffeeMachineInterface {
+    private OldCoffeeMachine OldVendingMachine;
+    
+    CoffeeTouchscreenAdapter(OldCoffeeMachine oldmachine){
+        this.OldVendingMachine = oldmachine;
+    }
+    
+    public void chooseFirstSelection() {
+        OldVendingMachine.selectA();
+    }
+    
+    public void chooseSecondSelection() {
+        OldVendingMachine.selectB();
+    }
+}
